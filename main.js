a7146555710fe67fca2210670c450c666d1596b8
eliasgrey9 <89663439+eliasgrey9@users.noreply.github.com>
2022-06-01 11:42:29 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -14,7 +14,7 @@
         rect.left >= 0 &&
         rect.bottom <=
           (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        rect.right <= (document.documentElement.clientWidth)
       );
     }
   
